70f131f6a3cd9281a11cdaa931016d7390f1ceb9
Shashank1324 <shashank.ls22558800@gmail.com>
2024-02-22 10:35:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -155,12 +155,7 @@ async def start(client, message):
                             [
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=f'https://t.me/{SUPPORT_CHAT}'),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                            ],[
-                                InlineKeyboardButton('𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥', url="https://t.me/Rockerssupport_bot")
-                            ],
-                            [
-                                InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @Rockerssupport_bot
-                            ]
+                           ]
                         ]
                     )
                 )
