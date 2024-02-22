d7d5bbba324c00cea0561db3104b2e9e40d0de4d
Shashank1324 <shashank.ls22558800@gmail.com>
2024-02-22 16:46:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -434,7 +434,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = '@TG_LINKS_CHANNEL  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    title = '@kannadarockers_guru  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
