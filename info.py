9cfa7633ef402ec77c331c8e8d35256cdc72a6e8
Shashank1324 <shashank.ls22558800@gmail.com>
2024-02-22 17:11:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -87,7 +87,7 @@ SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "seas
 
 
 # Online Stream and Download
-NO_PORT = bool(environ.get('NO_PORT', True))
+NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
