991f99e47da28ab4a68f41fd2414707e1e507c38
Shashank1324 <shashank.ls22558800@gmail.com>
2024-02-22 10:54:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -202,7 +202,7 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 <b>
 ╭─────── • ◆ • ───────╮
-🔅 Dᴀʀᴋ Mᴏᴅᴇ :  <a href="https://t.me/+knPw4WmxTvhiNTM9">Tᴏᴜᴄʜ</a>
+🔅 Dᴀʀᴋ Mᴏᴅᴇ :  <a href="https://t.me/+9WiE9kCNrIdiYTBl">Tᴏᴜᴄʜ</a>
 ╰─────── • ◆ • ───────╯
 ಗುರು,
  ಶೇರ್ ಮಾಡುವ ಮೂಲಕ ನಮಗೆ support ಮಾಡು ❤️
