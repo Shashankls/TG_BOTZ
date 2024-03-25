f2911fa519e7097576f8beb9a9711e68ec2b360d
Shashankls <125437933+Shashankls@users.noreply.github.com>
2024-03-25 12:30:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,6 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://Shashank2db:shashank.ls1324@cluster0.s4tzgbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Shashankls:shashank.ls1324@cluster0.htp1u2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
